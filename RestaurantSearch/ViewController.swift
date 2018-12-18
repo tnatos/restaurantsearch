99146834cb40dec00008b8bcf3b6fd817f90b10b
tnatos <iijmikm@darrens-macbook-pro.local>
2018-12-18 14:10:35 +0900

    Initial Commit

---
+++ RestaurantSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantSearch
+//
+//  Created by Darren Tang on 2018/12/18.
+//  Copyright © 2018 Darren Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
